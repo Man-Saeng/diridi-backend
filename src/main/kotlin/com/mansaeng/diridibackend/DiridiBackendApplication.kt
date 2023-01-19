eac42df2e339f6d188064650176f216f521b7d8e
d0lim <limdongyoung0@gmail.com>
2023-01-19 21:16:51 +0900

    init: repo

---
+++ src/main/kotlin/com/mansaeng/diridibackend/DiridiBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.mansaeng.diridibackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class DiridiBackendApplication
+
+fun main(args: Array<String>) {
+    runApplication<DiridiBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/mansaeng/diridibackend/DiridiBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.mansaeng.diridibackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DiridiBackendApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
