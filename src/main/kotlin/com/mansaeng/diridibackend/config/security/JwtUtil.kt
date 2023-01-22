b7a4973d9161fada87e8bec62d17389e9d312e01
d0lim <limdongyoung0@gmail.com>
2023-01-22 23:51:38 +0900

    feat: implement jwt util with only access token

---
+++ src/main/kotlin/com/mansaeng/diridibackend/config/security/JwtUtil.kt
@@ -0,0 +1,47 @@
+package com.mansaeng.diridibackend.config.security
+
+import com.mansaeng.diridibackend.entity.user.User
+import io.jsonwebtoken.Claims
+import io.jsonwebtoken.Jwts
+import io.jsonwebtoken.security.Keys
+import org.springframework.beans.factory.annotation.Value
+import org.springframework.stereotype.Component
+import java.nio.charset.StandardCharsets
+import java.security.Key
+import java.util.*
+import javax.annotation.PostConstruct
+
+@Component
+class JwtUtil(
+    @Value("\${jwt.access-secret}") private val accessSecret: String,
+    @Value("\${jwt.access-expiration}") private val accessExpiration: Long
+) {
+    private lateinit var accessKey: Key
+
+    @PostConstruct
+    fun init() {
+        accessKey = Keys.hmacShaKeyFor(accessSecret.toByteArray(StandardCharsets.UTF_8))
+    }
+
+    fun getAllClaimsFromAccessToken(accessToken: String): Claims =
+        Jwts.parserBuilder().setSigningKey(accessKey).build().parseClaimsJws(accessToken).body
+
+    fun getUserIdFromAccessToken(accessToken: String): Long = getAllClaimsFromAccessToken(accessToken).subject.toLong()
+
+    fun getExpirationFromAccessToken(accessToken: String): Date = getAllClaimsFromAccessToken(accessToken).expiration
+
+    private fun isTokenExpired(accessToken: String): Boolean = getExpirationFromAccessToken(accessToken).before(Date())
+
+    fun createAccessToken(user: User): String {
+        val claims = Jwts.claims().setSubject(user.id.toString())
+        val now = Date()
+        return Jwts.builder()
+            .setSubject(user.id.toString())
+            .setIssuedAt(now)
+            .setExpiration(Date(now.time + accessExpiration))
+            .signWith(accessKey)
+            .compact()
+    }
+
+    fun validateAccessToken(accessToken: String): Boolean = !isTokenExpired(accessToken)
+}
