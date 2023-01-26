de521044ba3dc218feecf8ece004360631b9f067
d0lim <limdongyoung0@gmail.com>
2023-01-26 20:52:39 +0900

    chore: remove ignored files

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,6 +18,7 @@ repositories {
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-webflux")
     implementation("org.springframework.boot:spring-boot-starter-security")
+    implementation("org.springframework.boot:spring-boot-starter-data-mongodb-reactive")
     implementation("com.fasterxml.jackson.module:jackson-module-kotlin")
     implementation("io.jsonwebtoken:jjwt-api:0.11.5")
     runtimeOnly("io.jsonwebtoken:jjwt-impl:0.11.5")
