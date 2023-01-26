052f39a067cdb7d8574f722df08419a3d83171b5
d0lim <limdongyoung0@gmail.com>
2023-01-26 21:38:24 +0900

    feat: add mongodb reactive repository

---
+++ src/main/kotlin/com/mansaeng/diridibackend/user/repository/UserRepository.kt
@@ -0,0 +1,9 @@
+package com.mansaeng.diridibackend.user.repository
+
+import com.mansaeng.diridibackend.entity.user.User
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+import reactor.core.publisher.Mono
+
+interface UserRepository : ReactiveMongoRepository<User, String> {
+    fun findByUsername(username: String): Mono<User>
+}
