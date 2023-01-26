e13c0b596543254951d020fe465a9559a0224b6f
d0lim <limdongyoung0@gmail.com>
2023-01-26 21:40:26 +0900

    feat: add service method with reactive mongo repository

--- src/main/kotlin/com/mansaeng/diridibackend/user/service/UserService.kt
+++ src/main/kotlin/com/mansaeng/diridibackend/user/service/UserService.kt
@@ -1,53 +1,30 @@
 package com.mansaeng.diridibackend.user.service
 
-import com.mansaeng.diridibackend.entity.user.Role
 import com.mansaeng.diridibackend.entity.user.User
+import com.mansaeng.diridibackend.user.repository.UserRepository
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Mono
-import reactor.kotlin.core.publisher.toMono
-import javax.annotation.PostConstruct
 
 @Service
-class UserService {
-    private var map: MutableMap<Long, User> = HashMap();
+class UserService(private val userRepository: UserRepository) {
 
-    @PostConstruct
-    fun postConstruct() {
-        // test 1234
-        // $2a$10$cCN/klyh5zqMKSXTAc3sYe7hPS/iHya07hFy6JpERlY20GDAS9aK6
-        map.put(
-            1,
-            User(
-                1,
-                "admin",
-                "\$2a\$10\$cCN/klyh5zqMKSXTAc3sYe7hPS/iHya07hFy6JpERlY20GDAS9aK6",
-                true,
-                listOf(Role.ROLE_ADMIN)
-            )
-        )
-        map.put(
-            2,
-            User(
-                2,
-                "userA",
-                "\$2a\$10\$cCN/klyh5zqMKSXTAc3sYe7hPS/iHya07hFy6JpERlY20GDAS9aK6",
-                true,
-                listOf(Role.ROLE_USER)
-            )
-        )
-        map.put(
-            3,
-            User(
-                3,
-                "userB",
-                "\$2a\$10\$cCN/klyh5zqMKSXTAc3sYe7hPS/iHya07hFy6JpERlY20GDAS9aK6",
-                true,
-                listOf(Role.ROLE_USER)
-            )
-        )
-    }
 
-    fun findById(id: Long): Mono<User> = map[id].toMono()
+//    @PostConstruct
+//    fun postConstruct() {
+//        // test 1234
+//        // $2a$10$cCN/klyh5zqMKSXTAc3sYe7hPS/iHya07hFy6JpERlY20GDAS9aK6
+//        userRepository.save(
+//            User(
+//                UUID.randomUUID().toString(),
+//                "userB",
+//                "\$2a\$10\$cCN/klyh5zqMKSXTAc3sYe7hPS/iHya07hFy6JpERlY20GDAS9aK6",
+//                true,
+//                listOf(Role.ROLE_USER)
+//            )
+//        ).subscribe { user -> println(user) }
+//    }
 
-    fun findByUsername(username: String): Mono<User> = map.values.find { user -> user.username == username }.toMono()
+    fun findById(id: String): Mono<User> = userRepository.findById(id)
+
+    fun findByUsername(username: String): Mono<User> = userRepository.findByUsername(username)
 }
