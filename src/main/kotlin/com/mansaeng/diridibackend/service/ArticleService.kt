35a9ec904ba9a1d6a528d4b1467d560645d2fcef
d0lim <limdongyoung0@gmail.com>
2023-02-04 02:09:57 +0900

    feat: implement ArticleService

---
+++ src/main/kotlin/com/mansaeng/diridibackend/service/ArticleService.kt
@@ -0,0 +1,18 @@
+package com.mansaeng.diridibackend.service
+
+import com.mansaeng.diridibackend.dto.CreateArticleRequest
+import com.mansaeng.diridibackend.entity.article.Article
+import com.mansaeng.diridibackend.entity.user.User
+import com.mansaeng.diridibackend.repository.ArticleRepository
+import org.springframework.stereotype.Service
+import reactor.core.publisher.Mono
+
+@Service
+class ArticleService(private val articleRepository: ArticleRepository) {
+
+    fun createArticle(writer: User, createArticleRequest: CreateArticleRequest): Mono<Article> {
+        val (title, description) = createArticleRequest
+        val article = Article(null, title, description, writer)
+        return articleRepository.save(article)
+    }
+}
