907306c4da124e1d0cc793750951caf88f537fa8
d0lim <limdongyoung0@gmail.com>
2023-02-04 21:34:58 +0900

    feat: add MongoAuditConfig for created and last modified

---
+++ src/main/kotlin/com/mansaeng/diridibackend/config/MongoAuditConfig.kt
@@ -0,0 +1,9 @@
+package com.mansaeng.diridibackend.config
+
+import org.springframework.context.annotation.Configuration
+import org.springframework.data.mongodb.config.EnableMongoAuditing
+
+@Configuration
+@EnableMongoAuditing
+class MongoAuditConfig {
+}
