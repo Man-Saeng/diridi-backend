3afbc58ed3c42bc3113a1426dc81a11d365084c9
d0lim <limdongyoung0@gmail.com>
2023-02-07 23:14:53 +0900

    refactor: modify repository method regarding relation and pagination

--- src/main/kotlin/com/mansaeng/diridibackend/repository/ArticleRepository.kt
+++ src/main/kotlin/com/mansaeng/diridibackend/repository/ArticleRepository.kt
@@ -1,7 +1,13 @@
 package com.mansaeng.diridibackend.repository
 
 import com.mansaeng.diridibackend.entity.article.Article
-import org.springframework.data.mongodb.repository.ReactiveMongoRepository
+import org.springframework.data.domain.Pageable
+import org.springframework.data.mongodb.repository.Query
+import org.springframework.data.repository.reactive.ReactiveCrudRepository
+import reactor.core.publisher.Flux
 
-interface ArticleRepository : ReactiveMongoRepository<Article, String> {
+
+interface ArticleRepository : ReactiveCrudRepository<Article, String> {
+    @Query("{ id: { \$exists: true }}")
+    fun findAllPaged(pageable: Pageable): Flux<Article>
 }
