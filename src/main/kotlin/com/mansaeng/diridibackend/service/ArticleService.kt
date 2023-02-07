5bac05e2a5a563df9d04185d521cce3da4697abb
d0lim <limdongyoung0@gmail.com>
2023-02-07 23:15:20 +0900

    feat: add getArticleList method to ArticleService

--- src/main/kotlin/com/mansaeng/diridibackend/service/ArticleService.kt
+++ src/main/kotlin/com/mansaeng/diridibackend/service/ArticleService.kt
@@ -4,7 +4,9 @@ import com.mansaeng.diridibackend.dto.CreateArticleRequest
 import com.mansaeng.diridibackend.entity.article.Article
 import com.mansaeng.diridibackend.entity.user.User
 import com.mansaeng.diridibackend.repository.ArticleRepository
+import org.springframework.data.domain.PageRequest
 import org.springframework.stereotype.Service
+import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @Service
@@ -12,7 +14,15 @@ class ArticleService(private val articleRepository: ArticleRepository) {
 
     fun createArticle(writer: User, createArticleRequest: CreateArticleRequest): Mono<Article> {
         val (title, description) = createArticleRequest
-        val article = Article(title = title, description = description, writer = writer)
+        val article = Article(title = title, description = description, writerId = writer.id)
         return articleRepository.save(article)
     }
+
+    fun getArticleList(skip: Int, take: Int): Flux<Article> {
+        val page = PageRequest.of(skip, take)
+
+        return articleRepository.findAllPaged(page)
+    }
+
+    fun getArticleDetailById(articleId: String): Mono<Article> = articleRepository.findById(articleId)
 }
