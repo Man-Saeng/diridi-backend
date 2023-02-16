2fde2f74a4a187201f9833f076ea761dc40b8c00
d0lim <limdongyoung0@gmail.com>
2023-02-16 23:37:34 +0900

    feat: update services

--- src/main/kotlin/com/mansaeng/diridibackend/service/EpisodeService.kt
+++ src/main/kotlin/com/mansaeng/diridibackend/service/EpisodeService.kt
@@ -1,6 +1,7 @@
 package com.mansaeng.diridibackend.service
 
-import com.mansaeng.diridibackend.dto.CreateEpisodeRequest
+import com.mansaeng.diridibackend.dto.request.CreateEpisodeRequest
+import com.mansaeng.diridibackend.dto.request.ModifyEpisodeRequest
 import com.mansaeng.diridibackend.entity.article.Episode
 import com.mansaeng.diridibackend.entity.user.User
 import com.mansaeng.diridibackend.repository.ArticleRepository
@@ -10,6 +11,7 @@ import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 import reactor.core.scheduler.Schedulers
+import java.time.LocalDateTime
 
 @Service
 class EpisodeService(
@@ -39,8 +41,27 @@ class EpisodeService(
 
     fun getEpisodeListByArticleId(articleId: String, skip: Int, take: Int): Flux<Episode> {
         val page = PageRequest.of(skip, take)
-
-        return episodeRepository.findAllPaged(articleId, page)
+        println(articleId)
+        return episodeRepository.findByArticleIdAndPublishedAtBefore(articleId, LocalDateTime.now(), page)
     }
 
+    fun getEpisodeDetail(episodeId: String): Mono<Episode> {
+        return episodeRepository.findById(episodeId)
+    }
+
+    fun modifyEpisode(writer: User, modifyEpisodeRequest: ModifyEpisodeRequest): Mono<Episode> {
+        val (episodeId, title, content, publishedAt) = modifyEpisodeRequest
+        return episodeRepository.findById(episodeId)
+            .map { episode ->
+                if (episode.writerId != writer.id) throw RuntimeException("자신이 쓴 게시물만 수정할 수 있습니다")
+                episode
+            }.map { episode ->
+                if (title != null) episode.title = title
+                if (content != null) episode.content = content
+                if (publishedAt != null) episode.publishedAt = publishedAt
+                episode
+            }.flatMap { episode ->
+                episodeRepository.save(episode)
+            }.switchIfEmpty(Mono.error(RuntimeException("존재하지 않는 episode id 입니다")))
+    }
 }
