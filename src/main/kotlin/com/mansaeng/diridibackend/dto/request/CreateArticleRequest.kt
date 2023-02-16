60086242b18e057951e30b225be986b25bd33e4d
d0lim <limdongyoung0@gmail.com>
2023-02-16 23:37:09 +0900

    feat: update dtos

--- src/main/kotlin/com/mansaeng/diridibackend/dto/request/CreateArticleRequest.kt
+++ src/main/kotlin/com/mansaeng/diridibackend/dto/request/CreateArticleRequest.kt
@@ -1,7 +1,10 @@
 package com.mansaeng.diridibackend.dto.request
 
+import com.mansaeng.diridibackend.entity.article.CategoryType
+
 data class CreateArticleRequest(
     val title: String,
     val description: String,
+    val category: CategoryType,
     val tags: List<String> = listOf()
 )
--- src/main/kotlin/com/mansaeng/diridibackend/dto/request/ModifyEpisodeRequest.kt
+++ src/main/kotlin/com/mansaeng/diridibackend/dto/request/ModifyEpisodeRequest.kt
@@ -2,9 +2,9 @@ package com.mansaeng.diridibackend.dto.request
 
 import java.time.LocalDateTime
 
-data class CreateEpisodeRequest(
-    val title: String,
-    val content: String,
+data class ModifyEpisodeRequest(
+    val episodeId: String,
+    val title: String?,
+    val content: String?,
     val publishedAt: LocalDateTime?,
-    val articleId: String
 )
