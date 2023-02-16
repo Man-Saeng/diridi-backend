22fcc65423199accec02b3f91fadcc62438852e3
d0lim <limdongyoung0@gmail.com>
2023-02-16 23:37:22 +0900

    feat: add method to repositories

--- src/main/kotlin/com/mansaeng/diridibackend/repository/ArticleRepository.kt
+++ src/main/kotlin/com/mansaeng/diridibackend/repository/ArticleRepository.kt
@@ -1,6 +1,7 @@
 package com.mansaeng.diridibackend.repository
 
 import com.mansaeng.diridibackend.entity.article.Article
+import com.mansaeng.diridibackend.entity.article.StatusType
 import org.springframework.data.domain.Pageable
 import org.springframework.data.mongodb.repository.Query
 import org.springframework.data.repository.reactive.ReactiveCrudRepository
@@ -11,5 +12,7 @@ interface ArticleRepository : ReactiveCrudRepository<Article, String> {
     @Query("{ id: { \$exists: true }}")
     fun findAllPaged(pageable: Pageable): Flux<Article>
 
-    fun findByTagsContaining(tag: String?, pageable: Pageable): Flux<Article>
+    fun findByStatusIsNot(status: StatusType, pageable: Pageable): Flux<Article>
+
+    fun findByTagsContainingAndStatusIsNot(tag: String?, status: StatusType, pageable: Pageable): Flux<Article>
 }
