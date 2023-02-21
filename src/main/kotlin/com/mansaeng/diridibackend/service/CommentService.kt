d7b1e81ee334142042be6b506302e65502d360fc
d0lim <limdongyoung0@gmail.com>
2023-02-21 22:05:47 +0900

    feat: add article id validation

--- src/main/kotlin/com/mansaeng/diridibackend/service/CommentService.kt
+++ src/main/kotlin/com/mansaeng/diridibackend/service/CommentService.kt
@@ -3,19 +3,27 @@ package com.mansaeng.diridibackend.service
 import com.mansaeng.diridibackend.dto.request.CreateCommentRequest
 import com.mansaeng.diridibackend.entity.comment.Comment
 import com.mansaeng.diridibackend.entity.user.User
+import com.mansaeng.diridibackend.repository.ArticleRepository
 import com.mansaeng.diridibackend.repository.CommentRepository
 import org.springframework.stereotype.Service
 import reactor.core.publisher.Flux
 import reactor.core.publisher.Mono
 
 @Service
-class CommentService(private val commentRepository: CommentRepository) {
+class CommentService(
+    private val commentRepository: CommentRepository,
+    private val articleRepository: ArticleRepository
+) {
 
     fun createComment(writer: User, createCommentRequest: CreateCommentRequest): Mono<String> {
         val (articleId, parentId, content) = createCommentRequest
 
-        val newComment = Comment(writerId = writer.id, parentId = parentId, content = content, articleId = articleId)
-        return commentRepository.save(newComment).mapNotNull { it.id }
+        return articleRepository.findById(articleId).switchIfEmpty(Mono.error(RuntimeException("No such article")))
+            .flatMap { _ ->
+                val newComment =
+                    Comment(writerId = writer.id, parentId = parentId, content = content, articleId = articleId)
+                commentRepository.save(newComment).mapNotNull { it.id }
+            }
     }
 
     fun getCommentsByArticleId(articleId: String, parentId: String?): Flux<Comment> {
